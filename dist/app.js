c081b8c05011d189fa26b39e2ad98fc7146c50a6
Kernelgarden <kernelgarden@gmail.com>
2019-06-12 12:11:14 +0900

    modify gitignore & add packages for test

--- dist/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const gp_bot_1 = require("./gp_bot");
-const ping_pong_1 = require("./handlers/ping_pong");
-const gpBot = new gp_bot_1.GPBot();
-let dispatcher = gpBot.Dispatcher();
-dispatcher.add_handler(ping_pong_1.ping_pong_handler);
-//# sourceMappingURL=app.js.map
